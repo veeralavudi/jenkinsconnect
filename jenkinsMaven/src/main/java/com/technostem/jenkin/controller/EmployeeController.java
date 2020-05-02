77d85216c3920bc4d75f718d3dcab664eb35857f
veera <veeralavudi@gmail.com>
2020-05-03 03:49:40 +0530

    error created compilation error resolved

--- jenkinsMaven/src/main/java/com/technostem/jenkin/controller/EmployeeController.java
+++ jenkinsMaven/src/main/java/com/technostem/jenkin/controller/EmployeeController.java
@@ -3,7 +3,11 @@ package com.technostem.jenkin.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 
+ * @author VEERA LAVUDI
+ *
+ */
 @RestController
 public class EmployeeController {
 
