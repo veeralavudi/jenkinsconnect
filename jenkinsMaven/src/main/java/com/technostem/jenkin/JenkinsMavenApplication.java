8a4d36c3deef3a9e84d3e188876dff63abcf8202
veera <veeralavudi@gmail.com>
2020-05-03 01:07:21 +0530

    jenkin hello world

---
+++ jenkinsMaven/src/main/java/com/technostem/jenkin/JenkinsMavenApplication.java
@@ -0,0 +1,13 @@
+package com.technostem.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMavenApplication.class, args);
+	}
+
+}
---
+++ jenkinsMaven/src/main/java/com/technostem/jenkin/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.technostem.jenkin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	@GetMapping("/employee/{name}")
+	public String hello(@PathVariable("name") String name) {
+		return "Hello   " + name;
+
+	}
+
+}
