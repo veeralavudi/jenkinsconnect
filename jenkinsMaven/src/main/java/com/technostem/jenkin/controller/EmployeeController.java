8c28ae7504900ece6b428c3036dd2d272b9553bf
veera <veeralavudi@gmail.com>
2020-05-03 02:00:25 +0530

    error created compilation error

--- jenkinsMaven/src/main/java/com/technostem/jenkin/controller/EmployeeController.java
+++ jenkinsMaven/src/main/java/com/technostem/jenkin/controller/EmployeeController.java
@@ -9,7 +9,7 @@ public class EmployeeController {
 
 	@GetMapping("/employee/{name}")
 	public String hello(@PathVariable("name") String name) {
-		Product product=new Product();
+		Product1 product=new Product1();
 		return "Hello   " + name;
 
 	}
